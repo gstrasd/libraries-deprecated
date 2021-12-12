2aafd893f0a88253fcb68e712b17d47541a0b3c6
Gary Strasdin <garystrasdin@gmail.com>
2021-12-11 21:07:15 -0600

    Created TableStorageClient

---
+++ src/Library.Platform/Storage/IDocumentStorageManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library.Platform.Storage
+{
+    public interface IDocumentStorageManager
+    {
+        Task<bool> StoreExistsAsync(string container, CancellationToken token = default);
+        Task CreateStoreAsync(string container, CancellationToken token = default);
+        Task DeleteStoreAsync(string container, CancellationToken token = default);
+        Task PurgeStoreAsync(string container, CancellationToken token = default);
+        IAsyncEnumerable<string> ListStoresAsync(CancellationToken token = default);
+    }
+}
---
+++ src/Library.Platform/Storage/ITableStorageClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Library.Platform.Storage
+{
+    public interface ITableStorageClient
+    {
+        Task<bool> ExistsAsync(object[] keys, CancellationToken token = default);
+        Task SaveAsync<T>(T entity, CancellationToken token = default);
+        Task<T> FindAsync<T>(object[] keys, CancellationToken token = default);
+        Task RemoveAsync(object[] keys, CancellationToken token = default);
+    }
+}
